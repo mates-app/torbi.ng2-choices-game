71b1ebaade2ee748f8829a931dc2784963d71bf5
josecullen <jose_cullen@hotmail.com>
2016-08-28 13:16:40 -0300

    angular2-quickstart-version

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by josecullen on 28/08/16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
+
